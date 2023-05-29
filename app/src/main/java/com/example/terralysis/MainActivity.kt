b83b1caeee7ade38bce582107707f6fd9136288f
asrorulkhopid <asroreal@gmail.com>
2023-05-29 09:27:59 +0800

    connecting bottom navigation with fragment

--- app/src/main/java/com/example/terralysis/MainActivity.kt
+++ app/src/main/java/com/example/terralysis/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.terralysis
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -19,5 +21,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val navView: BottomNavigationView = binding.navView
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navView.setupWithNavController(navController)
     }
 }
