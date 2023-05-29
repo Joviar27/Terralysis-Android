66aa380d10d02ecc5c7e20e0377ec68f1b73b269
Joviar27 <joviar27@gmail.com>
2023-05-30 04:17:49 +0700

    Create welcome fragment

---
+++ app/src/main/java/com/example/terralysis/ui/welcome/WelcomeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.terralysis.ui.welcome
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.terralysis.databinding.LayoutWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+    private var _binding: LayoutWelcomeBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = LayoutWelcomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
