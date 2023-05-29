d8a06b7e2d597fc998ff03914a6734de43f4e2b4
Joviar27 <joviar27@gmail.com>
2023-05-30 04:16:54 +0700

    Create Sign Up Fragment

---
+++ app/src/main/java/com/example/terralysis/ui/signup/SignUpFragment.kt
@@ -0,0 +1,31 @@
+package com.example.terralysis.ui.signup
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.terralysis.databinding.LayoutSigninBinding
+import com.example.terralysis.databinding.LayoutSignupBinding
+
+class SignUpFragment : Fragment() {
+    private var _binding: LayoutSignupBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = LayoutSignupBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
