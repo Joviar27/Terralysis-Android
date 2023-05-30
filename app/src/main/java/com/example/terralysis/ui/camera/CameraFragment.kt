fdf3934235ac4d904e87b760c12207f9d29fc72e
asrorulkhopid <asroreal@gmail.com>
2023-05-30 22:51:33 +0800

    add request permission functionality

--- app/src/main/java/com/example/terralysis/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/example/terralysis/ui/camera/CameraFragment.kt
@@ -1,9 +1,14 @@
 package com.example.terralysis.ui.camera
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.terralysis.R
@@ -13,6 +18,18 @@ class CameraFragment : Fragment() {
     private var _binding: ActivityCameraBinding? = null
 
     private val binding get() = _binding!!
+
+    private val requestPermissionsLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            if (permissions.all { it.value }) {
+                // start camera
+            } else {
+                // Permissions not granted
+                showToast("Tidak mendapatkan izin")
+                requireActivity().onBackPressedDispatcher.onBackPressed()
+                finishFragment()
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,8 +45,18 @@ class CameraFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        if(!allPermissionsGranted()) requestPermissionsLauncher.launch(REQUIRED_PERMISSIONS)
+
         backNavigation()
         takePicture()
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun finishFragment() {
+        parentFragmentManager.beginTransaction().remove(this).commit()
     }
 
     private fun takePicture(){
@@ -44,8 +71,20 @@ class CameraFragment : Fragment() {
         }
     }
 
+    private fun showToast(message: String){
+        Toast.makeText(
+            requireContext(),
+            message,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 }
