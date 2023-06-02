21c0ebaa14d789310b03f2a4071ab5a7c776ea6a
Joy Vianoktya <95416905+Joviar27@users.noreply.github.com>
2023-06-03 00:40:43 +0700

    Camera (#5)
    * add camera dependencies and permission
    
    * add request permission functionality
    
    * add utility functions and camera capture
    
    * add functionality to switch camera
    
    * add functionality to select image from galery
    
    * remove unused code
    
    ---------
    
    Co-authored-by: asrorulkhopid <asroreal@gmail.com>

--- app/src/main/java/com/example/terralysis/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/example/terralysis/ui/camera/CameraFragment.kt
@@ -1,18 +1,61 @@
 package com.example.terralysis.ui.camera
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.terralysis.R
 import com.example.terralysis.databinding.ActivityCameraBinding
+import com.example.terralysis.util.createTempFile
+import com.example.terralysis.util.rotateImage
+import com.example.terralysis.util.uriToFile
 
 class CameraFragment : Fragment() {
     private var _binding: ActivityCameraBinding? = null
 
     private val binding get() = _binding!!
+
+    private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+    private var imageCapture: ImageCapture? = null
+
+    private val requestPermissionsLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            if (permissions.all { it.value }) {
+                startCamera()
+            } else {
+                // Permissions not granted
+                showToast("Tidak mendapatkan izin")
+                requireActivity().onBackPressedDispatcher.onBackPressed()
+                finishFragment()
+            }
+        }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val selectedImg = result.data?.data as Uri
+            selectedImg.let { uri ->
+                val myFile = uriToFile(uri, requireContext())
+                navigateToDetail(myFile.absolutePath)
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,24 +71,106 @@ class CameraFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        backNavigation()
-        takePicture()
+        if (!allPermissionsGranted()) requestPermissionsLauncher.launch(REQUIRED_PERMISSIONS)
+        else startCamera()
+
+        binding.apply {
+            btnCapture.setOnClickListener { takePicture() }
+            btnSwitch.setOnClickListener { switchCamera() }
+            btnUpload.setOnClickListener { startGallery() }
+            btnBack.setOnClickListener { requireActivity().onBackPressedDispatcher.onBackPressed() }
+        }
     }
 
-    private fun takePicture(){
-        binding.btnCapture.setOnClickListener(
-            //logic for taking a picture
-            Navigation.createNavigateOnClickListener(R.id.action_CameraFragment_to_DetailFragment)
-        )
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
     }
-    private fun backNavigation(){
-        binding.btnBack.setOnClickListener {
-            requireActivity().onBackPressedDispatcher.onBackPressed()
-        }
+
+    private fun finishFragment() {
+        parentFragmentManager.beginTransaction().remove(this).commit()
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+            imageCapture = ImageCapture.Builder().build()
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview,
+                    imageCapture
+                )
+            } catch (exc: Exception) {
+                showToast("Gagal Memunculkan kamera")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun switchCamera() {
+        cameraSelector =
+            if (cameraSelector.equals(CameraSelector.DEFAULT_BACK_CAMERA)) CameraSelector.DEFAULT_FRONT_CAMERA
+            else CameraSelector.DEFAULT_BACK_CAMERA
+        startCamera()
+    }
+
+    private fun takePicture() {
+        val imageCapture = imageCapture ?: return
+        val photoFile = createTempFile(requireContext())
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(requireContext()),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    rotateImage(photoFile.absolutePath)
+                    navigateToDetail(photoFile.absolutePath)
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    showToast("Gagal mengambil gambar")
+                }
+            })
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Pilih gambar")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(
+            requireContext(),
+            message,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    //temp - navigate to detail
+    private fun navigateToDetail(imagePath: String) {
+        val bundle = Bundle()
+        bundle.putString("image_path", imagePath)
+        view?.findNavController()?.navigate(R.id.action_CameraFragment_to_DetailFragment, bundle)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 }
---
+++ app/src/main/java/com/example/terralysis/util/Utils.kt
@@ -0,0 +1,68 @@
+package com.example.terralysis.util
+
+import android.content.ContentResolver
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
+import android.net.Uri
+import android.os.Environment
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
+import java.text.SimpleDateFormat
+import java.util.*
+
+val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss",
+    Locale.US
+).format(System.currentTimeMillis())
+
+fun createTempFile(context: Context): File {
+    val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    return File.createTempFile(timeStamp, ".jpg", storageDir)
+}
+
+fun rotateImage(imagePath: String){
+    val bitmap = BitmapFactory.decodeFile(imagePath)
+    val exif = ExifInterface(imagePath)
+
+    val orientation = exif.getAttributeInt(
+        ExifInterface.TAG_ORIENTATION,
+        ExifInterface.ORIENTATION_UNDEFINED
+    )
+
+    val matrix = Matrix()
+
+    when (orientation) {
+        ExifInterface.ORIENTATION_ROTATE_90 -> matrix.postRotate(90f)
+        ExifInterface.ORIENTATION_ROTATE_180 -> matrix.postRotate(180f)
+        ExifInterface.ORIENTATION_ROTATE_270 -> matrix.postRotate(270f)
+    }
+
+    val rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+
+    val rotatedOutputStream = FileOutputStream(imagePath)
+    rotatedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, rotatedOutputStream)
+    rotatedOutputStream.close()
+
+}
+
+fun uriToFile(selectedImg: Uri, context: Context): File {
+    val contentResolver: ContentResolver = context.contentResolver
+    val myFile = createTempFile(context)
+
+    val inputStream = contentResolver.openInputStream(selectedImg) as InputStream
+    val outputStream: OutputStream = FileOutputStream(myFile)
+    val buf = ByteArray(1024)
+    var len: Int
+    while (inputStream.read(buf).also { len = it } > 0) outputStream.write(buf, 0, len)
+    outputStream.close()
+    inputStream.close()
+
+    return myFile
+}
+
+
+
