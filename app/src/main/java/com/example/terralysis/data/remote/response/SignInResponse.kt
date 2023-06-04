8e8f85415f8e6286dd7dbfeabf70b08030d19cc3
asrorulkhopid <asroreal@gmail.com>
2023-06-05 07:37:49 +0800

    update signup and signin response

--- app/src/main/java/com/example/terralysis/data/remote/response/SignInResponse.kt
+++ app/src/main/java/com/example/terralysis/data/remote/response/SignInResponse.kt
@@ -1,4 +1,30 @@
 package com.example.terralysis.data.remote.response
 
-class SignInResponse {
-}
+import com.google.gson.annotations.SerializedName
+
+data class SignInResponse(
+
+    @field:SerializedName("loginResult")
+    val loginResult: LoginResult? = null,
+
+    @field:SerializedName("error")
+    val error: Boolean? = null,
+
+    @field:SerializedName("message")
+    val message: String? = null
+)
+
+data class LoginResult(
+
+    @field:SerializedName("name")
+    val name: String? = null,
+
+    @field:SerializedName("userId")
+    val userId: String? = null,
+
+    @field:SerializedName("email")
+    val email: String? = null,
+
+    @field:SerializedName("token")
+    val token: String? = null
+)
--- app/src/main/java/com/example/terralysis/data/remote/response/SignUpResponse.kt
+++ app/src/main/java/com/example/terralysis/data/remote/response/SignUpResponse.kt
@@ -1,4 +1,13 @@
 package com.example.terralysis.data.remote.response
 
-class SignUpResponse {
-}
+import com.google.gson.annotations.SerializedName
+
+data class SignUpResponse(
+
+    @field:SerializedName("error")
+    val error: Boolean? = null,
+
+    @field:SerializedName("message")
+    val message: String? = null,
+    
+)
