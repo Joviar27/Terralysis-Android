290bc80a796fca2869aaa84531a949e5359eb726
Joviar27 <joviar27@gmail.com>
2023-06-06 15:23:00 +0700

    Add upload image feature

--- app/src/main/java/com/example/terralysis/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/example/terralysis/ui/camera/CameraFragment.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,20 +19,33 @@ import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.example.terralysis.R
-import com.example.terralysis.databinding.ActivityCameraBinding
+import com.example.terralysis.data.ResultState
+import com.example.terralysis.data.local.entity.ScanEntity
+
+import com.example.terralysis.databinding.LayoutCameraBinding
+import com.example.terralysis.util.ViewModelFactory
 import com.example.terralysis.util.createTempFile
 import com.example.terralysis.util.rotateImage
 import com.example.terralysis.util.uriToFile
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
 
 class CameraFragment : Fragment() {
-    private var _binding: ActivityCameraBinding? = null
+    private var _binding: LayoutCameraBinding? = null
+    private val binding get() = _binding
 
-    private val binding get() = _binding!!
+    private var _viewModel: CameraViewModel? = null
+    private val viewModel get() = _viewModel
 
     private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
     private var imageCapture: ImageCapture? = null
+
+    private var file : File? = null
 
     private val requestPermissionsLauncher =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
@@ -40,8 +54,7 @@ class CameraFragment : Fragment() {
             } else {
                 // Permissions not granted
                 showToast("Tidak mendapatkan izin")
-                requireActivity().onBackPressedDispatcher.onBackPressed()
-                finishFragment()
+                view?.findNavController()?.navigateUp()
             }
         }
 
@@ -52,20 +65,28 @@ class CameraFragment : Fragment() {
             val selectedImg = result.data?.data as Uri
             selectedImg.let { uri ->
                 val myFile = uriToFile(uri, requireContext())
-                navigateToDetail(myFile.absolutePath)
+                rotateImage(myFile.absolutePath)
+                file = myFile
+                uploadImage()
             }
         }
+        else{
+            showToast(resources.getString(R.string.failed_gallery_message))
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        obtainViewModel()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-
-        _binding = ActivityCameraBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        return root
+    ): View? {
+        _binding = LayoutCameraBinding.inflate(inflater, container, false)
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -74,20 +95,16 @@ class CameraFragment : Fragment() {
         if (!allPermissionsGranted()) requestPermissionsLauncher.launch(REQUIRED_PERMISSIONS)
         else startCamera()
 
-        binding.apply {
+        binding?.apply {
             btnCapture.setOnClickListener { takePicture() }
             btnSwitch.setOnClickListener { switchCamera() }
             btnUpload.setOnClickListener { startGallery() }
-            btnBack.setOnClickListener { requireActivity().onBackPressedDispatcher.onBackPressed() }
+            btnBack.setOnClickListener { view.findNavController().navigateUp() }
         }
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun finishFragment() {
-        parentFragmentManager.beginTransaction().remove(this).commit()
     }
 
     private fun startCamera() {
@@ -97,13 +114,13 @@ class CameraFragment : Fragment() {
             val preview = Preview.Builder()
                 .build()
                 .also {
-                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                    it.setSurfaceProvider(binding?.viewFinder?.surfaceProvider)
                 }
             imageCapture = ImageCapture.Builder().build()
             try {
                 cameraProvider.unbindAll()
                 cameraProvider.bindToLifecycle(
-                    this,
+                     this,
                     cameraSelector,
                     preview,
                     imageCapture
@@ -116,7 +133,7 @@ class CameraFragment : Fragment() {
 
     private fun switchCamera() {
         cameraSelector =
-            if (cameraSelector.equals(CameraSelector.DEFAULT_BACK_CAMERA)) CameraSelector.DEFAULT_FRONT_CAMERA
+            if (cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) CameraSelector.DEFAULT_FRONT_CAMERA
             else CameraSelector.DEFAULT_BACK_CAMERA
         startCamera()
     }
@@ -133,11 +150,11 @@ class CameraFragment : Fragment() {
             object : ImageCapture.OnImageSavedCallback {
                 override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
                     rotateImage(photoFile.absolutePath)
-                    navigateToDetail(photoFile.absolutePath)
+                    file = photoFile
+                    uploadImage()
                 }
-
                 override fun onError(exception: ImageCaptureException) {
-                    showToast("Gagal mengambil gambar")
+                    showToast(resources.getString(R.string.failed_capture_message))
                 }
             })
     }
@@ -146,8 +163,52 @@ class CameraFragment : Fragment() {
         val intent = Intent()
         intent.action = Intent.ACTION_GET_CONTENT
         intent.type = "image/*"
-        val chooser = Intent.createChooser(intent, "Pilih gambar")
+        val chooser = Intent.createChooser(intent, resources.getString(R.string.chooser))
         launcherIntentGallery.launch(chooser)
+    }
+
+    private fun uploadImage() {
+        if(file != null){
+            val myFile = file as File
+
+            //For later on if need compressing
+            //val compressedFile = reduceFileImage(myFile)
+
+            val requestImageFile = myFile.asRequestBody("image/jpeg".toMediaTypeOrNull())
+
+            val imageMultipart : MultipartBody.Part = MultipartBody.Part.createFormData(
+                "photo",
+                myFile.name,
+                requestImageFile
+            )
+            viewModel?.requestScan(imageMultipart)
+            getScanResult()
+        }
+    }
+
+    private fun getScanResult(){
+        viewModel?.scan?.observe(viewLifecycleOwner){ result ->
+            when(result){
+                is ResultState.Loading -> {
+                    showLoading(true)
+                }
+                is ResultState.Error ->{
+                    showLoading(false)
+                    showToast(resources.getString(R.string.failed_upload_message))
+                    Log.e(TAG, result.error)
+                }
+                is ResultState.Success ->{
+                    showLoading(false)
+                    //Send image id to detail page
+                    navigateToDetail(result.data)
+                }
+            }
+        }
+    }
+
+    private fun showLoading(isLoading : Boolean){
+        binding?.progressContainer?.visibility = if (isLoading) View.VISIBLE else View.GONE
+        binding?.progressMessage?.text = resources.getString(R.string.process_message)
     }
 
     private fun showToast(message: String) {
@@ -158,11 +219,19 @@ class CameraFragment : Fragment() {
         ).show()
     }
 
+    private fun obtainViewModel() {
+        val factory : ViewModelFactory = ViewModelFactory.getInstance(requireActivity())
+        val viewModel : CameraViewModel by viewModels {
+            factory
+        }
+        _viewModel = viewModel
+    }
+
     //temp - navigate to detail
-    private fun navigateToDetail(imagePath: String) {
-        val bundle = Bundle()
-        bundle.putString("image_path", imagePath)
-        view?.findNavController()?.navigate(R.id.action_CameraFragment_to_DetailFragment, bundle)
+    private fun navigateToDetail(scan : ScanEntity) {
+        val toDetailScan = CameraFragmentDirections.actionCameraFragmentToDetailFragment(scan)
+        toDetailScan.scanDetail = scan
+        view?.findNavController()?.navigate(toDetailScan)
     }
 
     override fun onDestroyView() {
@@ -172,5 +241,6 @@ class CameraFragment : Fragment() {
 
     companion object {
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val TAG = "CameraFragment"
     }
 }
