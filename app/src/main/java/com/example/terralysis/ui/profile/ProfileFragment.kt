2892de6183dba5d2eebb0bf6fffefd70c89fd59c
Joviar27 <joviar27@gmail.com>
2023-06-06 15:24:28 +0700

    Show user data

--- app/src/main/java/com/example/terralysis/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/terralysis/ui/profile/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.terralysis.ui.profile
 
+import android.content.res.Resources
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,29 +11,34 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import com.example.terralysis.R
+import com.example.terralysis.databinding.LayoutProfileBinding
+import com.example.terralysis.util.ViewModelFactory
+import java.util.*
 import com.example.terralysis.data.ResultState
 import com.example.terralysis.databinding.LayoutProfilePageBinding
 import com.example.terralysis.ui.AuthActivity
 import com.example.terralysis.util.ViewModelFactory
 
 class ProfileFragment : Fragment() {
-    private var _binding: LayoutProfilePageBinding? = null
+    private var _binding: LayoutProfileBinding? = null
+    private val binding get() = _binding
 
-    private val binding get() = _binding!!
+    private var _viewModel: ProfileViewModel? = null
+    private val viewModel get() = _viewModel
 
-    private val viewModel: ProfileViewModel by viewModels() {
-        ViewModelFactory.getInstance(requireContext())
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        obtainViewModel()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
 
-        _binding = LayoutProfilePageBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        return root
+        _binding = LayoutProfileBinding.inflate(inflater, container, false)
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -56,37 +62,43 @@ class ProfileFragment : Fragment() {
     }
 
     private fun setContent(){
-        viewModel.getUserData().observe(viewLifecycleOwner){ result ->
-            when (result){
-                is ResultState.Loading ->{ }
-                is ResultState.Error ->{ }
-                is ResultState.Success -> {
-                    result.data.let {
-                        binding.tvEmail.text = it?.email
-                        binding.tvUsername.text = it?.name
-                    }
-                }
-            }
+        //Temp - until found default pic
+        binding?.ivProfile?.setImageResource(R.color.secondary)
+
+        viewModel?.getUserData()?.observe(viewLifecycleOwner){ user ->
+            binding?.tvUsername?.text = user.name
+            binding?.tvEmail?.text = user.email
         }
 
-        binding.itemBahasa.apply {
+        binding?.itemBahasa?.apply {
             mtrlListItemText.text = resources.getText(R.string.language)
-            mtrlListItemSecondaryText.text = "Indonesia"
+            mtrlListItemSecondaryText.text = getLocaleLanguage()
         }
-        binding.itemBantuan.apply {
+        binding?.itemBantuan?.apply {
             mtrlListItemIcon.setImageResource(R.drawable.outline_contact_support_24)
             mtrlListItemText.text = resources.getText(R.string.bantuan)
             mtrlListItemSecondaryText.visibility = View.GONE
         }
-        binding.itemAboutApp.apply {
+        binding?.itemAboutApp?.apply {
             mtrlListItemIcon.setImageResource(R.drawable.outline_info_24)
             mtrlListItemText.text = resources.getText(R.string.about_app)
             mtrlListItemSecondaryText.visibility = View.GONE
         }
     }
 
+    private fun getLocaleLanguage(): String {
+        val currentLocale: Locale = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            Resources.getSystem().configuration.locales[0]
+        } else {
+            @Suppress("Deprecation")
+            Resources.getSystem().configuration.locale
+        }
+
+        return currentLocale.language
+    }
+
     private fun setNavigation(){
-        binding.apply {
+        binding?.apply {
             itemBantuan.mtrlListItemNavigation.setOnClickListener(
                 Navigation.createNavigateOnClickListener(R.id.action_profileFragment_to_bantuanFragment)
             )
@@ -104,6 +116,13 @@ class ProfileFragment : Fragment() {
         requireActivity().finish()
     }
 
+    private fun obtainViewModel() {
+        val factory : ViewModelFactory = ViewModelFactory.getInstance(requireActivity())
+        val viewModel : ProfileViewModel by viewModels {
+            factory
+        }
+        _viewModel = viewModel
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
