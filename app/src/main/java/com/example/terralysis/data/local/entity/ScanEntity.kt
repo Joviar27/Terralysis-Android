1d970243a5c9002c4906d2a34cd79d06a6e44044
Joviar27 <joviar27@gmail.com>
2023-06-06 15:04:14 +0700

    Parcelize the entity

--- app/src/main/java/com/example/terralysis/data/local/entity/ScanEntity.kt
+++ app/src/main/java/com/example/terralysis/data/local/entity/ScanEntity.kt
@@ -1,9 +1,12 @@
 package com.example.terralysis.data.local.entity
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity(tableName = "scans")
 data class ScanEntity(
     @PrimaryKey
@@ -24,4 +27,4 @@ data class ScanEntity(
 
     @field:ColumnInfo(name = "shortDesc")
     val shortDesc : String
-)
+) : Parcelable
