f0fc6dcb5c7cde3b80445a1be6016350accb54be
Joviar27 <joviar27@gmail.com>
2023-06-10 21:59:15 +0700

    Add carousel

--- app/src/main/java/com/example/terralysis/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/terralysis/ui/home/HomeFragment.kt
@@ -9,10 +9,11 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import com.example.terralysis.R
-import com.example.terralysis.data.ResultState
 import com.example.terralysis.databinding.LayoutHomeBinding
 import com.example.terralysis.ui.AuthActivity
+import com.example.terralysis.util.CarouselAdapter
 import com.example.terralysis.util.ViewModelFactory
+import com.google.android.material.carousel.CarouselLayoutManager
 
 class HomeFragment : Fragment() {
 
@@ -38,6 +39,7 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         checkAuth()
+        setupView()
         setUser()
         navigateToCamera()
         navigateToGuide()
@@ -51,9 +53,19 @@ class HomeFragment : Fragment() {
 
     private fun setUser() {
         viewModel.getUserData().observe(viewLifecycleOwner) { result ->
-            binding.tvWelcomeName.text =
-                resources.getString(R.string.home_welcome_message, result.name)
+            binding.tvWelcomeName.text = resources.getString(R.string.home_welcome_message, result.name)
         }
+    }
+
+    private fun setupView(){
+        val imageList: List<Int> = listOf(
+            R.drawable.carousel_1,
+            R.drawable.carousel_2,
+            R.drawable.carousel_3
+        )
+        val adapter = CarouselAdapter(imageList)
+        binding.rvCarousel.layoutManager = CarouselLayoutManager()
+        binding.rvCarousel.adapter = adapter
     }
 
     private fun startAuthActivity() {
