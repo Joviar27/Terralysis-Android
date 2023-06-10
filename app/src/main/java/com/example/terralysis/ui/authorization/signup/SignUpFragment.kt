473bbf478142e1aa6b119945c99fed77eada4df9
Joviar27 <joviar27@gmail.com>
2023-06-10 22:07:44 +0700

    Update textview validation

--- app/src/main/java/com/example/terralysis/ui/authorization/signup/SignUpFragment.kt
+++ app/src/main/java/com/example/terralysis/ui/authorization/signup/SignUpFragment.kt
@@ -1,5 +1,6 @@
 package com.example.terralysis.ui.authorization.signup
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,8 +8,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.findNavController
 import com.example.terralysis.R
 import com.example.terralysis.data.ResultState
 import com.example.terralysis.databinding.LayoutSignupBinding
@@ -16,8 +16,7 @@ import com.example.terralysis.util.ViewModelFactory
 
 class SignUpFragment : Fragment() {
     private var _binding: LayoutSignupBinding? = null
-
-    private val binding get() = _binding!!
+    private val binding get() = _binding
 
     private val viewModel: SignUpViewModel by viewModels {
         ViewModelFactory.getInstance(requireContext())
@@ -27,63 +26,79 @@ class SignUpFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-
+    ): View? {
         _binding = LayoutSignupBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        return root
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupView()
+    }
 
-        binding.apply {
+    private fun setupView(){
+        binding?.apply {
             btnSignin.setOnClickListener {
                 val name = etName.text.toString()
                 val email = etEmail.text.toString()
                 val password = etPassword.text.toString()
-                submitForm(name, email, password)
+
+                clearError()
+                when {
+                    name.isEmpty() -> binding?.etlName?.error = resources.getString(R.string.err_required)
+                    email.isEmpty() -> binding?.etlEmail?.error = resources.getString(R.string.err_required)
+                    !isValidEmail(email) -> binding?.etlEmail?.error = resources.getString(R.string.err_invalid_email)
+                    password.isEmpty() -> binding?.etlPassword?.error = resources.getString(R.string.err_required)
+                    password.length <8 -> binding?.etlPassword?.error = resources.getString(R.string.err_invalid_pass_1)
+                    password.contains(" ") -> binding?.etlPassword?.error = resources.getString(R.string.err_invalid_pass_2)
+                    else -> signUp(name, email, password)
+                }
+            }
+            tvSignIn.setOnClickListener {
+                navigateToSignIn()
             }
         }
     }
 
-    private fun submitForm(name: String, email: String, password: String){
-        when {
-            name.isEmpty() -> binding.etName.error =
-                resources.getString(R.string.err_required)
-            email.isEmpty() -> binding.etlEmail.error =
-                resources.getString(R.string.err_required)
-            !isValidEmail(email) -> binding.etlEmail.error =
-                resources.getString(R.string.err_invalid_email)
-            password.isEmpty() -> binding.etlPassword.error =
-                resources.getString(R.string.err_required)
-            else -> {
-                viewModel.signUp(name, email, password).observe(viewLifecycleOwner) { result ->
-                    when (result) {
-                        is ResultState.Loading -> binding.pbLoading.visibility = View.VISIBLE
-                        is ResultState.Error -> {
-                            binding.pbLoading.visibility = View.GONE
-                            Toast.makeText(requireContext(), result.error, Toast.LENGTH_SHORT).show()
-                        }
-                        is ResultState.Success -> {
-                            binding.pbLoading.visibility = View.GONE
-                            Toast.makeText(requireContext(), result.data, Toast.LENGTH_SHORT).show()
-                            findNavController().navigate(R.id.action_signUpFragment_to_signInFragment)
-                        }
-                    }
+    private fun navigateToSignIn(){
+        val toSignIn = SignUpFragmentDirections.actionNavigationSignupToNavigationSignin()
+        view?.findNavController()?.navigate(toSignIn)
+    }
+
+    private fun clearError(){
+        binding?.apply {
+            etlEmail.error = null
+            etlPassword.error = null
+            etlName.error = null
+        }
+    }
+
+    private fun signUp(name: String, email: String, password: String){
+        viewModel.signUp(name, email, password).observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is ResultState.Loading -> showLoading(true)
+                is ResultState.Error -> {
+                    showLoading(false)
+                    showToast(requireContext(), resources.getString(R.string.signup_failed))
+                }
+                is ResultState.Success -> {
+                    showLoading(false)
+                    showToast(requireContext(), resources.getString(R.string.signup_succcess))
+                    navigateToSignIn()
                 }
             }
         }
     }
 
-    private fun isValidEmail(email: String) = android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
-
-    private fun findNavController(): NavController {
-        val fragmentManager = requireParentFragment().parentFragmentManager
-        val navHostFragment = fragmentManager.findFragmentById(R.id.nav_host_fragment_activity_auth) as NavHostFragment
-        val navController = navHostFragment.navController
-        return navController
+    private fun showLoading(isLoading : Boolean){
+        binding?.pbLoading?.visibility = if (isLoading) View.VISIBLE else View.GONE
     }
+
+    private fun showToast(context: Context, message : String){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun isValidEmail(email: String) = android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
 
     override fun onDestroyView() {
         super.onDestroyView()
