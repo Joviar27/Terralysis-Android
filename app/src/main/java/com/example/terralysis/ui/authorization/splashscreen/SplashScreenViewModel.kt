c798e4a7b073005d8d3d9c45684db6b4b458767c
Joviar27 <joviar27@gmail.com>
2023-06-11 02:13:08 +0700

    Add check auth function

--- app/src/main/java/com/example/terralysis/ui/authorization/splashscreen/SplashScreenViewModel.kt
+++ app/src/main/java/com/example/terralysis/ui/authorization/splashscreen/SplashScreenViewModel.kt
@@ -1,10 +1,15 @@
 package com.example.terralysis.ui.authorization.splashscreen
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.example.terralysis.data.local.entity.AuthEntity
 import com.example.terralysis.data.repository.AuthRepository
 
 class SplashScreenViewModel(
     private val authRepository: AuthRepository
 ) : ViewModel() {
-    fun checkAuthState() = authRepository.checkAuthState()
+    fun getAuthData() : LiveData<AuthEntity> {
+        return authRepository.checkAuthState().asLiveData()
+    }
 }
