31138134e795a6bf781070fddc7875c2973b71ac
Joviar27 <joviar27@gmail.com>
2023-06-11 02:10:57 +0700

    Remove unused auth function

--- app/src/main/java/com/example/terralysis/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/terralysis/ui/home/HomeFragment.kt
@@ -1,6 +1,5 @@
 package com.example.terralysis.ui.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +9,6 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import com.example.terralysis.R
 import com.example.terralysis.databinding.LayoutHomeBinding
-import com.example.terralysis.ui.AuthActivity
 import com.example.terralysis.util.CarouselAdapter
 import com.example.terralysis.util.ViewModelFactory
 import com.google.android.material.carousel.CarouselLayoutManager
@@ -18,8 +16,7 @@ import com.google.android.material.carousel.CarouselLayoutManager
 class HomeFragment : Fragment() {
 
     private var _binding: LayoutHomeBinding? = null
-
-    private val binding get() = _binding!!
+    private val binding get() = _binding
 
     private val viewModel: HomeViewModel by viewModels {
         ViewModelFactory.getInstance(requireContext())
@@ -29,31 +26,23 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-
+    ): View? {
         _binding = LayoutHomeBinding.inflate(inflater, container, false)
-        return binding.root
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        checkAuth()
         setupView()
         setUser()
         navigateToCamera()
         navigateToGuide()
     }
 
-    private fun checkAuth() {
-        viewModel.getAuthData().observe(viewLifecycleOwner) { result ->
-            if (!result.state) startAuthActivity()
-        }
-    }
-
     private fun setUser() {
         viewModel.getUserData().observe(viewLifecycleOwner) { result ->
-            binding.tvWelcomeName.text = resources.getString(R.string.home_welcome_message, result.name)
+            binding?.tvWelcomeName?.text = resources.getString(R.string.home_welcome_message, result.name)
         }
     }
 
@@ -64,25 +53,18 @@ class HomeFragment : Fragment() {
             R.drawable.carousel_3
         )
         val adapter = CarouselAdapter(imageList)
-        binding.rvCarousel.layoutManager = CarouselLayoutManager()
-        binding.rvCarousel.adapter = adapter
-    }
-
-    private fun startAuthActivity() {
-        val authIntent = Intent(requireContext(), AuthActivity::class.java)
-        authIntent.putExtra(AuthActivity.isSplash, false)
-        startActivity(authIntent)
-        requireActivity().finish()
+        binding?.rvCarousel?.layoutManager = CarouselLayoutManager()
+        binding?.rvCarousel?.adapter = adapter
     }
 
     private fun navigateToCamera() {
-        binding.btnScan.setOnClickListener(
+        binding?.btnScan?.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_navigation_home_to_navigation_camera)
         )
     }
 
     private fun navigateToGuide() {
-        binding.btnAboutApps.setOnClickListener(
+        binding?.btnAboutApps?.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_navigation_home_to_navigation_guide)
         )
     }
--- app/src/main/java/com/example/terralysis/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/terralysis/ui/home/HomeViewModel.kt
@@ -3,19 +3,15 @@ package com.example.terralysis.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import com.example.terralysis.data.local.entity.AuthEntity
 import com.example.terralysis.data.local.entity.UserEntity
 import com.example.terralysis.data.repository.AuthRepository
 
 class HomeViewModel (
     private val authRepository: AuthRepository
     ) : ViewModel() {
+
     fun getUserData() : LiveData<UserEntity> {
         return authRepository.getUserData().asLiveData()
-    }
-
-    fun getAuthData() : LiveData<AuthEntity> {
-        return authRepository.checkAuthState().asLiveData()
     }
 
 }
