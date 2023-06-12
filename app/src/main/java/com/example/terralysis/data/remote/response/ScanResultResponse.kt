47285b430e2519c6a0b3b810c72bde04651a1339
Joviar27 <joviar27@gmail.com>
2023-06-12 23:30:39 +0700

    Update response

--- app/src/main/java/com/example/terralysis/data/remote/response/ScanResultResponse.kt
+++ app/src/main/java/com/example/terralysis/data/remote/response/ScanResultResponse.kt
@@ -2,44 +2,65 @@ package com.example.terralysis.data.remote.response
 
 import com.google.gson.annotations.SerializedName
 
-/*Gajadi dipake, dialihkan ke ScanRequestResponse
-
 data class ScanResultResponse(
 
-	@field:SerializedName("data")
-	val data: DataResult,
+	@field:SerializedName("image")
+	val image: Image,
 
-	@field:SerializedName("error")
-	val error: Boolean,
+	@field:SerializedName("status_code")
+	val statusCode: Int,
 
 	@field:SerializedName("message")
 	val message: String
 )
 
-data class DataResult(
-
-	@field:SerializedName("createdAt")
-	val createdAt: String,
+data class Image(
 
 	@field:SerializedName("imageId")
 	val imageId: String,
 
-	@field:SerializedName("size")
-	val size: Int,
-
 	@field:SerializedName("originalname")
 	val originalname: String,
 
-	@field:SerializedName("mimetype")
-	val mimetype: String,
+	@field:SerializedName("ciri_fisik")
+	val ciriFisik: String,
+
+	@field:SerializedName("ciri_morfologi")
+	val ciriMorfologi: String,
 
 	@field:SerializedName("userId")
 	val userId: String,
 
+	@field:SerializedName("kandungan")
+	val kandungan: String,
+
 	@field:SerializedName("url")
 	val url: String,
+
+	@field:SerializedName("long_desc")
+	val longDesc: String,
+
+	@field:SerializedName("persebaran")
+	val persebaran: String,
+
+	@field:SerializedName("createdAt")
+	val createdAt: String,
+
+	@field:SerializedName("size")
+	val size: Int,
+
+	@field:SerializedName("kelas")
+	val kelas: String,
+
+	@field:SerializedName("mimetype")
+	val mimetype: String,
+
+	@field:SerializedName("short_desc")
+	val shortDesc: String,
+
+	@field:SerializedName("ciri_kimia")
+	val ciriKimia: String,
 
 	@field:SerializedName("updatedAt")
 	val updatedAt: String
 )
- */
