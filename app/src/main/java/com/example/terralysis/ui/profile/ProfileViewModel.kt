aae52c9e168aa256a8b360a48fdf8e0d57add01d
Joviar27 <joviar27@gmail.com>
2023-06-12 23:37:16 +0700

    Add function to delete all data in local database

--- app/src/main/java/com/example/terralysis/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/terralysis/ui/profile/ProfileViewModel.kt
@@ -7,10 +7,12 @@ import androidx.lifecycle.viewModelScope
 import com.example.terralysis.data.local.entity.AuthEntity
 import com.example.terralysis.data.local.entity.UserEntity
 import com.example.terralysis.data.repository.AuthRepository
+import com.example.terralysis.data.repository.ScanRepository
 import kotlinx.coroutines.launch
 
 class ProfileViewModel(
-    private val authRepository: AuthRepository
+    private val authRepository: AuthRepository,
+    private val scanRepository: ScanRepository
 ) : ViewModel() {
     fun getUserData() : LiveData<UserEntity> {
         return authRepository.getUserData().asLiveData()
@@ -19,8 +21,14 @@ class ProfileViewModel(
         return authRepository.checkAuthState().asLiveData()
     }
 
+    fun deleteLocalData(){
+        viewModelScope.launch {
+            scanRepository.deleteAllScans()
+        }
+    }
+
     fun logout(){
-        viewModelScope.launch{
+        viewModelScope.launch {
             authRepository.logout()
         }
     }
