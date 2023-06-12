0ac326fd8af503468ab964dab1d3f06dcc815e15
Joviar27 <joviar27@gmail.com>
2023-06-12 23:36:09 +0700

    update detail view to show new entity data

--- app/src/main/java/com/example/terralysis/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/terralysis/ui/detail/DetailFragment.kt
@@ -1,19 +1,32 @@
 package com.example.terralysis.ui.detail
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.terralysis.R
+import com.example.terralysis.data.ResultState
 import com.example.terralysis.data.local.entity.ScanEntity
 import com.example.terralysis.databinding.LayoutDetailBinding
+import com.example.terralysis.util.ViewModelFactory
 
 class DetailFragment : Fragment() {
     private var _binding: LayoutDetailBinding? = null
     private val binding get() = _binding
+
+    private var _viewModel: DetailViewModel? = null
+    private val viewModel get() = _viewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        obtainViewModel()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,26 +41,74 @@ class DetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val scanDetail = DetailFragmentArgs.fromBundle(arguments as Bundle).scanDetail
+
+        viewModel?.getUserData()?.observe(viewLifecycleOwner){ user ->
+            getScanDetail(user.userId, scanDetail.id)
+        }
+
         setupView(scanDetail)
     }
 
     private fun setupView(scanDetail : ScanEntity){
-        binding?.tvDetailLabel?.text = scanDetail.name
-        binding?.tvDetailShortDesc?.text = scanDetail.shortDesc
-        binding?.tvDetailLongDesc?.text = scanDetail.longDesc
-        binding?.tvDetailLongDesc2?.text = scanDetail.longDesc
+        binding?.apply {
+            tvDetailLabel.text = resources.getString(R.string.label,scanDetail.name, scanDetail.shortDesc)
+            tvDetailLongdesc.text = scanDetail.longDesc
+            tvDetailProperty.text = scanDetail.property
+            tvDetailSpread.text = scanDetail.spread
+            tvDetailPhysicalChar.text = scanDetail.physicalCar
+            tvDetailMorphologyChar.text = scanDetail.morphologyChar
+            tvDetailChemicalChar.text = scanDetail.chemicalChar
 
-        binding?.ivDetail?.let {
-            Glide.with(requireActivity())
-                .load(scanDetail.uri)
-                .placeholder(R.color.primaryContainer)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .into(it)
+            ivDetail.let {
+                Glide.with(requireActivity())
+                    .load(scanDetail.uri)
+                    .placeholder(R.color.primaryContainer)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(it)
+            }
         }
+    }
+
+    private fun getScanDetail(userId : String, imageId : String){
+        viewModel?.getScanDetail(userId, imageId)?.observe(viewLifecycleOwner){ result ->
+            when (result) {
+                is ResultState.Loading -> showLoading(true)
+                is ResultState.Error -> {
+                    showToast(resources.getString(R.string.err_api_problem))
+                    Log.e(TAG, result.error)
+                    showLoading(false)
+                }
+                is ResultState.Success -> {
+                    showLoading(false)
+                    val data = result.data
+                    setupView(data)
+                }
+            }
+        }
+    }
+
+    private fun showLoading(isLoading : Boolean){
+        binding?.progressBar?.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
+    private fun showToast(message : String){
+        Toast.makeText(requireContext(),message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun obtainViewModel() {
+        val factory : ViewModelFactory = ViewModelFactory.getInstance(requireActivity())
+        val viewModel : DetailViewModel by viewModels {
+            factory
+        }
+        _viewModel = viewModel
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object{
+        private const val TAG = "DetailFragment"
     }
 }
